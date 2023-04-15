03735eef6f3d5665aa569661322333b5820198f8
Chris Smith Jr <89213845+ChrisS561@users.noreply.github.com>
2023-04-15 12:16:43 -0500

    Create financialmanager.py

---
+++ FinancialManger.py
@@ -0,0 +1,4 @@
+#Budgeting tool, allows user to enter income and expenses and calculates the difference
+#Savings tool, allows users to enter how much they want to save and how long they want to save for and calculates how much they need to save each month
+#Investment tool, allows users to enter how much they want to invest and how long they want to invest for and calculates how much they need to invest each month
+#Retirement tool, allows users to enter how much they want to retire with and how long they want to retire for and calculates how much they need to save each month
